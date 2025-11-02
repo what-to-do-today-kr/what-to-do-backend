2330e2d0da8db9c095db83aed6b184ce30f33891
leegwichan <cnddkscndgus@naver.com>
2025-11-03 08:45:29 +0900

    feat: initialize project

---
+++ src/main/kotlin/keo/whattodo/WhatToDoApplication.kt
@@ -0,0 +1,11 @@
+package keo.whattodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatToDoApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatToDoApplication>(*args)
+}
---
+++ src/test/kotlin/keo/whattodo/WhatToDoApplicationTests.kt
@@ -0,0 +1,13 @@
+package keo.whattodo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhatToDoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
